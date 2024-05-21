72120cbb031c6288c326b9bc9bfcbe4c9c4b5853
flikru <frostt315@gmail.com>
2024-05-21 22:16:14 +0500

    Update clearTableSession.php

--- clearTableSession.php
+++ clearTableSession.php
@@ -1,3 +1,5 @@
 Если в базе данных, таблица b_user_session занимает много места, можно ее очистить выполнив код в командной php строке
 Код
 \Bitrix\Main\Session\Handlers\Table\UserSessionTable::deleteOlderThan(86400);
+Командная php строка находится в админке сайта по пути
+АДРЕС_САЙТА/bitrix/admin/php_command_line.php?lang=ru
